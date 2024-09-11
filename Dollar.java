5dbcf7b8db437ac83e4636cbf52ce962fee15082
Renanlucass <renanlucas0014@gmail.com>
2024-09-11 10:52:43 -0300

    Classe Dollar

---
+++ Dollar.java
@@ -0,0 +1,13 @@
+class Dollar extends Money {
+    private int amount;
+    Dollar(int amount) {
+       this.amount = amount;
+    }
+    Money times(int multiplier) {
+       return new Dollar(amount * multiplier);
+    }
+    public boolean equals(Object object) {
+       Dollar dollar = (Dollar) object;
+       return amount == dollar.amount;
+    }
+ }
